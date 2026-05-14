ab3dc5dcf76a0d35dfefc50a0d89ca43752228dd
Lucar-aster <luca.r@astercucine.it>
2026-05-14 11:54:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ with st.sidebar:
         scelta_c = st.selectbox("Per quale commessa?", list(nomi_c.keys()))
         nome_o = st.text_input("Nome Sottogruppo/Ordine")
         if st.button("Salva Ordine"):
-            conn.table("ordini").insert({"commessa_id": nomi_c[scelta_c], "nome_ordine": nome_o}).execute()
+            conn.table("ordini").insert({"commessa_id": nomi_c[scelta_c], "nome": nome_o}).execute()
             st.success("Ordine salvato!")
 
 # --- AREA PRINCIPALE: REGISTRAZIONE DOCUMENTO ---
@@ -34,7 +34,7 @@ st.header("📂 Registra Documento su più Ordini")
 
 # 1. Seleziona Ordini (anche di commesse diverse se vuoi)
 tutti_ordini = conn.table("ordini").select("id, nome_ordine").execute()
-dizionario_ordini = {o['nome_ordine']: o['id'] for o in tutti_ordini.data}
+dizionario_ordini = {o['nome']: o['id'] for o in tutti_ordini.data}
 
 ordini_scelti = st.multiselect("Seleziona uno o più Ordini/Sottogruppi", list(dizionario_ordini.keys()))
 
@@ -44,12 +44,12 @@ path_doc = st.text_input("Percorso locale (es. D:/Lavori/file.pdf)")
 if st.button("Indicizza Documento"):
     if ordini_scelti and nome_doc and path_doc:
         # Inserisce il documento
-        res = conn.table("documenti").insert({"nome_documento": nome_doc, "percorso_locale": path_doc}).execute()
+        res = conn.table("documenti").insert({"nome": nome_doc, "percorso": path_doc}).execute()
         doc_id = res.data[0]['id']
         
         # Inserisce i collegamenti nella tabella ponte
         for o_nome in ordini_scelti:
-            conn.table("documento_ordini").insert({"documento_id": doc_id, "ordine_id": dizionario_ordini[o_nome]}).execute()
+            conn.table("ponte").insert({"documenti_id": doc_id, "ordine_id": dizionario_ordini[o_nome]}).execute()
         
         st.success("Documento indicizzato correttamente!")
     else:
@@ -63,9 +63,9 @@ ordine_filtro = st.selectbox("Scegli un ordine per vedere i file associati", lis
 if ordine_filtro:
     o_id = dizionario_ordini[ordine_filtro]
     # Query complessa: trova i documenti collegati a questo ordine
-    docs = conn.table("documento_ordini").select("documenti(nome_documento, percorso_locale)").eq("ordine_id", o_id).execute()
+    docs = conn.table("ponte").select("documenti(nome, percorso)").eq("ordine_id", o_id).execute()
     
     for item in docs.data:
         d = item['documenti']
-        st.write(f"📄 **{d['nome_documento']}**")
-        st.code(d['percorso_locale'])
+        st.write(f"📄 **{d['nome']}**")
+        st.code(d['percorso'])
